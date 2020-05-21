88c212e2c9ab2a03c530d9c5753e87fb8731e16b
MikeADev <mikearcherdev@gmail.com>
2020-05-21 14:51:32 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="row">
+        <span className="heading">Testing</span>Testing
+        <span className="subheading">Testing</span>
+        <span className="quote">Testing</span>
+        <span className="title">Testing</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
