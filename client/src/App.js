6dcfc04b0380f6757587c38eb595959dbce5cacb
MikeADev <mikearcherdev@gmail.com>
2020-05-24 18:49:16 -0500

    implement navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Cart from './components/Cart';
+import Default from './components/Default';
+import Details from './components/Details';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="row">
-        <span className="heading">Testing</span>Testing
-        <span className="subheading">Testing</span>
-        <span className="quote">Testing</span>
-        <span className="title">Testing</span>
-      </div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Navbar />
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/details" component={Details}/>
+            <Route path="/cart" component={Cart}/>
+            <Route component={Default}/>
+          </Switch>
+        </main>
+      </Router>
+      
+    );
+  }
 }
 
 export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <nav className="navbar">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link to={`/`}>Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={`/`}>About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={`/products`}>Products</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={`/contact`}>Contact</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={`/cart`}>Cart</Link>
+                    </li>
+                </ul>
+            </nav>
         )
     }
 }
